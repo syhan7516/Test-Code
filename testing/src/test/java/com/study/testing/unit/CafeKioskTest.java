994d947a8ae5ba85b94246c8085f0aa95dc4f481
신영한 <syhan7516@naver.com>
2025-05-08 19:54:20 +0900

    키오스크 자동화 테스트 수행
    - 메뉴 추가 테스트
    - 메뉴 삭제 테스트
    - 초기화 테스트
    - hasSize, isEmpty 사용법 확인

--- testing/src/test/java/com/study/testing/unit/CafeKioskTest.java
+++ testing/src/test/java/com/study/testing/unit/CafeKioskTest.java
@@ -1,12 +1,15 @@
 package com.study.testing.unit;
 
 import com.study.testing.unit.beverage.Americano;
+import com.study.testing.unit.beverage.Latte;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class CafeKioskTest {
 
     @Test
-    void add() {
+    void add_manual_test() {
         CafeKiosk cafeKiosk = new CafeKiosk();
         cafeKiosk.add(new Americano());
 
@@ -14,4 +17,38 @@ class CafeKioskTest {
         System.out.println(">>> 담긴 음료 : " + cafeKiosk.getBeverages().get(0).getName());
     }
 
+    @Test
+    void add() {
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        cafeKiosk.add(new Americano());
+
+        assertThat(cafeKiosk.getBeverages()).hasSize(1);
+        assertThat(cafeKiosk.getBeverages().get(0).getName()).isEqualTo("아메리카노");
+    }
+
+    @Test
+    void remove() {
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        Americano americano = new Americano();
+
+        cafeKiosk.add(americano);
+        assertThat(cafeKiosk.getBeverages()).hasSize(1);
+
+        cafeKiosk.remove(americano);
+        assertThat(cafeKiosk.getBeverages()).isEmpty();
+    }
+
+    @Test
+    void clear() {
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        Americano americano = new Americano();
+        Latte latte = new Latte();
+
+        cafeKiosk.add(americano);
+        cafeKiosk.add(latte);
+        assertThat(cafeKiosk.getBeverages()).hasSize(2);
+
+        cafeKiosk.clear();
+        assertThat(cafeKiosk.getBeverages()).isEmpty();
+    }
 }
