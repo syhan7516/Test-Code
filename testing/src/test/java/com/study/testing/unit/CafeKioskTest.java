aacb7127ff392a903e534e96dbec9e4427722200
신영한 <syhan7516@naver.com>
2025-05-08 19:23:10 +0900

    키오스크 음료 추가 수동 테스트 수행

---
+++ testing/src/test/java/com/study/testing/unit/CafeKioskTest.java
@@ -0,0 +1,17 @@
+package com.study.testing.unit;
+
+import com.study.testing.unit.beverage.Americano;
+import org.junit.jupiter.api.Test;
+
+class CafeKioskTest {
+
+    @Test
+    void add() {
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        cafeKiosk.add(new Americano());
+
+        System.out.println(">>> 담긴 음료 수 : " + cafeKiosk.getBeverages().size());
+        System.out.println(">>> 담긴 음료 : " + cafeKiosk.getBeverages().get(0).getName());
+    }
+
+}
